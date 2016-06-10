d0910a7c0fdd64edbf36042fe6b707f77c69ba43
Paulo Marquez Herrero <me@pmarquezh.info>
2016-06-10 22:33:45 +0100

    2016-06-10 22:23 - Summer Cleaning, part two. - commit

--- PERSONA-HEIMDALL/src/main/java/com/fxt/config/ConfigParams.java
+++ PERSONA-HEIMDALL/src/main/java/com/fxt/config/ConfigParams.java
@@ -5,9 +5,6 @@ package com.fxt.config;
  * @author pmarquez
  */
 public class ConfigParams {
-
-    public static final String DSN = "jdbc/aph2021";
-//    public static final String DSN = "java:comp/env/jdbc/aph2021";
     
 //   Regeneración de Contraseñas - BEGIN
     public static final String KEY_PWD_REGEN_REQ_TIEMPO_EXPIRACION_EN_HORAS = "KEY_PWD_REGEN_REQ_TIEMPO_EXPIRACION_EN_HORAS";
